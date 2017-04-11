9ad841eeda5898abac03a6795a13d50d3f668d69
Gigarion <cdgum@princeton.edu>
2017-04-10 20:49:54 -0400

    Added ActorStorage

---
+++ src/Actors/ActorStorage.java
@@ -0,0 +1,14 @@
+package Actors;
+
+import java.io.Serializable;
+
+/**
+ * Created by Gig on 4/10/2017.
+ * Serializable storage class for saving actor info in files
+ * Particularly useful for the Mapbuilder's actor additions.
+ *
+ * May refactor mailroom to send these instead
+ */
+public class ActorStorage implements Serializable {
+
+}
